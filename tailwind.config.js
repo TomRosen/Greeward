7c0888f36ae0305c2f7ad60e535cd9f496677fe6
Jannis Hajda <jannis.hajda@protonmail.com>
2022-03-20 00:30:36 +0100

    added tailwind, pictures, navbar

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: "#e4fff6",
+          100: "#daffec",
+          200: "#d0ffe2",
+          300: "#c6ffd8",
+          400: "#bcfbce",
+          500: "#b2f1c4",
+          600: "#a8e7ba",
+          700: "#9eddb0",
+          800: "#94d3a6",
+          900: "#8ac99c",
+        },
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+  ],
+};
