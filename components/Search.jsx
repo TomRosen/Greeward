8d5c757ff0c6cd4b79afe806e654f30013653f1e
hafidz <h.arifin@campus.tu-berlin.de>
2022-03-20 00:51:54 +0100

    add engagement, Search, ProductList

---
+++ components/Search.jsx
@@ -0,0 +1,30 @@
+import { SearchIcon, GlobeAltIcon } from '@heroicons/react/solid';
+
+export default function Example() {
+	return (
+		<div className='flex justify-center mt-6'>
+			<div className='mt-1 flex rounded-md shadow-sm w-6/12 justify-center'>
+				<div className='relative flex items-stretch flex-grow focus-within:z-10'>
+					<div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+						<GlobeAltIcon
+							className='h-5 w-5 text-gray-400'
+							aria-hidden='true'
+						/>
+					</div>
+					<input
+						type='text'
+						name='search'
+						id='search'
+						className='focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md pl-10 sm:text-sm border-gray-300'
+						placeholder='Type here for search'
+					/>
+				</div>
+				<button
+					type='button'
+					className='-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
+					<SearchIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
+				</button>
+			</div>
+		</div>
+	);
+}
