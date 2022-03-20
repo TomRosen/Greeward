a585f07f8c61d26934b46ab96df24d86bb2a94f7
Jannis Hajda <jannis.hajda@protonmail.com>
2022-03-20 09:19:42 +0100

    Changed ProductList to class

--- components/ProductHighlightList.jsx
+++ components/ProductHighlightList.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const products = [
   {
     id: 1,
@@ -41,31 +43,33 @@ const products = [
   },
 ];
 
-export default function ProductList() {
-  return (
-    <div className="px-4 py-6 sm:px-6 lg:px-8">
-      <h1 className="text-xl font-semibold text-gray-900">Highlights</h1>
-      <div className="bg-white">
-        <div className="max-w-2xl mx-auto py-16 px-4 sm:py-8 sm:px-6 lg:max-w-7xl lg:px-8">
-          <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {products.map((product) => (
-              <a key={product.id} href={product.href} className="group">
-                <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-5">
-                  <img
-                    src={product.imageSrc}
-                    alt={product.imageAlt}
-                    className="w-full h-full object-center object-cover group-hover:opacity-75"
-                  />
-                </div>
-                <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-                <p className="mt-1 text-lg font-medium text-gray-900">
-                  {product.price}
-                </p>
-              </a>
-            ))}
+export default class ProductList extends React.Component {
+  render() {
+    return (
+      <div className="px-4 py-6 sm:px-6 lg:px-8">
+        <h1 className="text-xl font-semibold text-gray-900">Highlights</h1>
+        <div className="bg-white">
+          <div className="max-w-2xl mx-auto py-16 px-4 sm:py-8 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+              {products.map((product) => (
+                <a key={product.id} href={product.href} className="group">
+                  <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-5">
+                    <img
+                      src={product.imageSrc}
+                      alt={product.imageAlt}
+                      className="w-full h-full object-center object-cover group-hover:opacity-75"
+                    />
+                  </div>
+                  <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+                  <p className="mt-1 text-lg font-medium text-gray-900">
+                    {product.price}
+                  </p>
+                </a>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
