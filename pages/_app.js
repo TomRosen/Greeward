e5420eefb864a62fef10ff9b83fd67f57b3f41b6
Tom Rosen <55321560+TomRosen@users.noreply.github.com>
2022-03-20 12:53:18 +0100

    Remove react query devtools

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,22 @@
 import '../styles/globals.css';
-import {SessionProvider} from 'next-auth/react';
-import {QueryClient, QueryClientProvider} from 'react-query';
-import {ReactQueryDevtools} from 'react-query/devtools';
+import { SessionProvider } from 'next-auth/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import Navbar from '@/components/Navbar';
 
 const queryClient = new QueryClient();
 
-function MyApp({Component, pageProps: {session, ...pageProps}}) {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <SessionProvider session={session}>
-                <Navbar></Navbar>
-                <div className="max-w-5xl mx-auto">
-                    <Component {...pageProps} />
-                    <ReactQueryDevtools initialIsOpen={false}/>
-                </div>
-            </SessionProvider>
-        </QueryClientProvider>
-    );
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<SessionProvider session={session}>
+				<Navbar></Navbar>
+				<div className='max-w-5xl mx-auto'>
+					<Component {...pageProps} />
+				</div>
+			</SessionProvider>
+		</QueryClientProvider>
+	);
 }
 
 export default MyApp;
