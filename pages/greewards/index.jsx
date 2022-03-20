2a97e9cf39f057781ecd99aa15db71ab9471be4b
Jannis Hajda <jannis.hajda@protonmail.com>
2022-03-20 11:20:55 +0100

    added database greeward

--- pages/greewards/index.jsx
+++ pages/greewards/index.jsx
@@ -1,11 +1,30 @@
 import Search from "@/components/Search";
 import ProductListGreewards from "@/components/ProductListGreewards";
+import { useQueryClient, useQuery } from "react-query";
+import { getGreewardsAll } from "@/helper/data/greewards";
+import { useEffect } from "react/cjs/react.production.min";
 
 const Home = ({}) => {
+  const queryClient = useQueryClient();
+  const { data, error, status } = useQuery("greewards", () =>
+    getGreewardsAll()
+  );
+
   return (
     <div>
       <Search></Search>
-      <ProductListGreewards></ProductListGreewards>
+      {status !== "loading" ? (
+        <ProductListGreewards products={data}></ProductListGreewards>
+      ) : (
+        <div class="flex justify-center items-center">
+          <div
+            class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
+            role="status"
+          >
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
